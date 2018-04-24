134dc9b04c7b507bf979ccacbed694f3389b03f6
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-04-24 08:10:16 -0400

    Restoring parallelism for trustymail but reducing it completely for sslyze

--- scan.sh
+++ scan.sh
@@ -18,13 +18,22 @@ redis-cli -h orchestrator_redis_1 del gathering_complete
 # Run the scans
 echo "Running domain-scan scan"
 cd $SHARED_DIR/artifacts/
-/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=pshtt --lambda --debug --meta --cache --workers=550
+/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv \
+                               --scan=pshtt,trustymail \
+                               --lambda --debug --meta --cache --workers=550
 # domain-scan removes all existing CSV result files before starting a
-# new scan, so we need to stash the pshtt results in a safe place
-mv $SHARED_DIR/artifacts/results/pshtt.csv $SHARED_DIR/artifacts/pshtt.csv
-/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv --scan=trustymail,sslyze --lambda --debug --meta --cache --workers=50
-# Now put the pshtt results back
-mv $SHARED_DIR/artifacts/pshtt.csv $SHARED_DIR/artifacts/results/pshtt.csv
+# new scan, so we need to stash the pshtt and trustymail results in a
+# safe place
+mv $SHARED_DIR/artifacts/results/pshtt.csv \
+   $SHARED_DIR/artifacts/results/trustymail.csv \
+   $SHARED_DIR/artifacts/
+/home/scanner/domain-scan/scan $SHARED_DIR/artifacts/scanme.csv \
+                               --scan=sslyze \
+                               --lambda --debug --meta --cache --workers=1
+# Now put the pshtt and trustymail results back
+mv $SHARED_DIR/artifacts/pshtt.csv \
+   $SHARED_DIR/artifacts/trustymail.csv \
+   $SHARED_DIR/artifacts/results/
 
 # Let redis know we're done
 redis-cli -h orchestrator_redis_1 set scanning_complete true
